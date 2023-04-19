90c3238b139613ae490fbe3db74688bc7edc4403
Yunseop Kim <c.o.d.e@kakao.com>
2023-04-19 22:18:30 +0900

    queue 7

--- src/queue.js
+++ src/queue.js
@@ -1,21 +1,25 @@
-var queue_items = [];
-var working = false;
+function Queue() {
+    var queue_items = [];
+    var working = false;
 
-function runNext() {
-    if (working)
-        return;
-    if (queue_items.length === 0)
-        return;
-    working = true;
-    var cart = queue_items.shift();
-    calc_cart_total(cart, function (total) {
-        update_total_dom(total);
-        working = false;
-        runNext();
-    });
+    function runNext() {
+        if (working)
+            return;
+        if (queue_items.length === 0)
+            return;
+        working = true;
+        var cart = queue_items.shift();
+        calc_cart_total(cart, function (total) {
+            update_total_dom(total);
+            working = false;
+            runNext();
+        });
+    }
+
+    return function (cart) {
+        queue_items.push(cart);
+        setTimeout(runNext, 0);
+    };
 }
 
-function update_total_queue(cart) {
-    queue_items.push(cart);
-    setTimeout(runNext, 0);
-}
+var update_total_queue = Queue();
